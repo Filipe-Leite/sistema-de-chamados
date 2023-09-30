a7aaddc5b77c1796129de655019212ddfda9daf4
filipe-leite <filipedasilva94.1@gmail.com>
2023-09-30 00:15:20 -0300

    Lesson 116 finished - table grom RECALL page working

--- sistema-de-chamados/src/pages/Customers/index.tsx
+++ sistema-de-chamados/src/pages/Customers/index.tsx
@@ -4,15 +4,38 @@ import Title from '../../components/Title';
 
 import { FiUser } from 'react-icons/fi'
 
+import { db } from '../../services/firebaseConnection';
+import { addDoc, collection } from 'firebase/firestore';
+
+import { toast } from 'react-toastify';
+
 export default function Customers(){
     const [nome, setNome] = useState('');
     const [cnpj, setCnpj] = useState('');
     const [endereco, setEndereco] = useState('');
 
-    function handleRegister(event: any){
+    async function handleRegister(event: any){
         event.preventDefault();
         
-        alert("teste")
+        if(nome !== '' && cnpj !== '' && endereco !== ''){
+            await addDoc(collection(db,"customers"), {
+                nomeFantasia: nome,
+                cnpj: cnpj,
+                endereco: endereco
+            })
+            .then(() => {
+                setNome('');
+                setCnpj('');
+                setEndereco('');
+                toast.success("Empresa registrada!");
+            })
+            .catch((error) => {
+                console.log(error)
+                toast.error("Erro ao fazer o cadastro.")
+            })
+        }else{
+            toast.error("Preencha todos os campos!")
+        }
     }
 
     return(
@@ -23,34 +46,37 @@ export default function Customers(){
                 <Title name="Clientes">
                     < FiUser size={25} />
                 </Title>
-            </div>
 
-            <div className='container'>
-                <form className='form-profile' onClick={handleRegister}>
-                    <label>Nome fantasia</label>
-                    <input
-                        type="text"
-                        placeholder='Nome da empresa'
-                        value={nome}
-                        onChange={(event) => setNome(event.target.value)}
-                    />
+                <div className='container'>
+                    <form className='form-profile' onSubmit={handleRegister}>
+                        <label>Nome fantasia</label>
+                        <input
+                            type="text"
+                            placeholder='Nome da empresa'
+                            value={nome}
+                            onChange={(event) => setNome(event.target.value)}
+                        />
 
-                    <label>CNPJ</label>
-                    <input
-                        type="text"
-                        placeholder='Digite o CNPJ'
-                        value={cnpj}
-                        onChange={(event) => setCnpj(event.target.value)}
-                    />
+                        <label>CNPJ</label>
+                        <input
+                            type="text"
+                            placeholder='Digite o CNPJ'
+                            value={cnpj}
+                            onChange={(event) => setCnpj(event.target.value)}
+                        />
 
-                    <label>Endereço</label>
-                    <input
-                        type="text"
-                        placeholder='Endereço da empresa'
-                        value={endereco}
-                        onChange={(event) => setEndereco(event.target.value)}
-                    />
-                </form>
+                        <label>Endereço</label>
+                        <input
+                            type="text"
+                            placeholder='Endereço da empresa'
+                            value={endereco}
+                            onChange={(event) => setEndereco(event.target.value)}
+                        />
+                        <button type='submit'>
+                            Salvar
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     )
--- sistema-de-chamados/src/pages/Dashboard/index.tsx
+++ sistema-de-chamados/src/pages/Dashboard/index.tsx
@@ -1,9 +1,16 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
+
 import Header from "../../components/Header";
+import Title from "../../components/Title";
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit, FiEdit2 } from "react-icons/fi";
+
+import { Link } from 'react-router-dom';
+
+import './dashboard.css';
 
 export default function Dashboard(){
-    const { logout } = useContext(AuthContext)
+    const { logout } = useContext(AuthContext);
     
     async function handleLogout(){
         await logout();
@@ -12,8 +19,75 @@ export default function Dashboard(){
     return(
         <div>
             <Header/>
-            <h1>Pagina Dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+            
+            <div className="content">
+                <Title nome="Tickets">
+                    <FiMessageSquare size={25}/>
+                </Title>
+
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#FFF" size={25}/>
+                        Novo chamado
+                    </Link>
+                    
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Mercado Esquina</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{ backgroundColor: '#999'}}>
+                                        Em aberto
+                                    </span>
+                                </td>
+                                <td data-label="Cadastrado">12/05/2022</td>
+                                <td data-label="#">
+                                    <button className="action" style={{backgroundColor: '#3583f6' }}>
+                                        <FiSearch color='#FFF' size={17}/>
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 color='#FFF' size={17}/>
+                                    </button>
+                                </td>
+                            </tr>
+
+
+
+
+
+                            <tr>
+                                <td data-label="Cliente">Informática Tech</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{ backgroundColor: '#999'}}>
+                                        Em aberto
+                                    </span>
+                                </td>
+                                <td data-label="Cadastrado">12/05/2022</td>
+                                <td data-label="#">
+                                    <button className="action" style={{backgroundColor: '#3583f6' }}>
+                                        <FiSearch color='#FFF' size={17}/>
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 color='#FFF' size={17}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+
+            </div>
         </div>
     )
 }
